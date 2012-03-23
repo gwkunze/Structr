61b23ed015bc1748c7ba05112e8914c3ee0ac554
Remon van de Kamp <rpkamp@gmail.com>
2012-03-23 16:44:27 +0100

    Don't create a new \DateTime if  is empty

--- src/Structr/Tree/Scalar/DateTimeNode.php
+++ src/Structr/Tree/Scalar/DateTimeNode.php
@@ -12,7 +12,7 @@ class DateTimeNode extends Node
     protected function _walk_value($value) {
         $value = parent::_walk_value($value);
 
-        if (!($value instanceof \DateTime))
+        if (!($value instanceof \DateTime) && !empty($value))
         {
             try
             {
