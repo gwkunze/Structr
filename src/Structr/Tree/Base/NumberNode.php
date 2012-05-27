46a56ae2220696047d7817b749547962300607c4
Remon van de Kamp <rpkamp@gmail.com>
2012-05-27 19:03:38 +0200

    Added comments to numbernode, and refactored it
    - Instead of _hasGt and _compareGt, etc, just use _gt which is false
      initially and set to a value when needed
    - Throw an exception when high >= low in clamp
    - Split out all tests in _walk_value to their own methods
      for readability

--- src/Structr/Tree/Base/NumberNode.php
+++ src/Structr/Tree/Base/NumberNode.php
@@ -6,88 +6,239 @@ use Structr\Tree\Base\ScalarNode;
 
 use Structr\Exception;
 
+/**
+ * Base functionality for all scalar nodes that
+ * are concerned with numbers
+ */
 abstract class NumberNode extends ScalarNode
 {
-
-    private $_hasGt = false;
-    private $_hasGte = false;
-    private $_hasLt = false;
-    private $_hasLte = false;
-    private $_compareGt;
-    private $_compareGte;
-    private $_compareLt;
-    private $_compareLte;
+    /**
+     * @var mixed The value of this node is expected to be greater than
+     *     this value
+     */
+    private $_gt = false;
+    
+    /**
+     * @var mixed The value of this node is expected to be greater than or equal
+     *     to this value
+     */
+    private $_gte = false;
+    
+    /**
+     * @var mixed The value of this node is expected to be less than this value
+     */
+    private $_lt = false;
+    
+    /**
+     * @var mixed The value of this node is expected to be less than or equal to
+     *     this value
+     */
+    private $_lte = false;
+    
+    /**
+     * @var bool Whether to clamp the value if it is greater than the value of
+     *    _gte
+     */
     private $_clampGte = false;
+    
+    /**
+     * @var bool Whether to clamp the value if it is less than the value of
+     *    _lte
+     */
     private $_clampLte = false;
 
-    public function gt($value) {
-        $this->_hasGt = true;
-        $this->_compareGt = $value;
+    /**
+     * The value of this node is expected to be greater than a fixed value
+     * 
+     * @param type $value
+     * @return \Structr\Tree\Base\NumberNode
+     */
+    public function gt($value)
+    {
+        $this->_gt = $value;
 
         return $this;
     }
 
-    public function gte($value, $clamp = false) {
-        $this->_hasGte = true;
-        $this->_compareGte = $value;
+    /**
+     * The value of this node is expected to be greater than or equal to 
+     *     a fixed value
+     * 
+     * @param type $value
+     * @param type $clamp
+     * @return \Structr\Tree\Base\NumberNode
+     */
+    public function gte($value, $clamp = false)
+    {
+        $this->_gte = $value;
         $this->_clampGte = $clamp;
 
         return $this;
     }
 
-    public function lt($value) {
-        $this->_hasLt = true;
-        $this->_compareLt = $value;
+    /**
+     * The value of this node is expected to be less than a fixed value
+     * 
+     * @param type $value
+     * @return \Structr\Tree\Base\NumberNode
+     */
+    public function lt($value)
+    {
+        $this->_lt = $value;
 
         return $this;
     }
 
-    public function lte($value, $clamp = false) {
-        $this->_hasLte = true;
-        $this->_compareLte = $value;
+    /**
+     * The value of this node is expected to be less than or equal to a
+     *     fixed value
+     * 
+     * @param type $value
+     * @param type $clamp
+     * @return \Structr\Tree\Base\NumberNode
+     */
+    public function lte($value, $clamp = false)
+    {
+        $this->_lte = $value;
         $this->_clampLte = $clamp;
 
         return $this;
     }
 
-    public function clamp($low, $high) {
+    /**
+     * The value of this node is expected to be between two fixed values. 
+     * If it isn't, it will be forced within the interval.
+     * 
+     * @param type $low
+     * @param type $high
+     * @return \Structr\Tree\Base\NumberNode
+     */
+    public function clamp($low, $high)
+    {
+        if ($high <= $low) {
+            throw new Exception(
+                'Low value must be stricly lower than high value for clamping'
+            );
+        }
         $this->gte($low, true);
         $this->lte($high, true);
 
         return $this;
     }
 
-    public function _walk_value($value) {
+    /**
+     * {@inheritdoc}
+     * @throws Structr\Exception
+     */
+    public function _walk_value($value)
+    {
         $value = parent::_walk_value($value);
 
-        if ($this->_hasGte) {
-            if($value < $this->_compareGte && !$this->_clampGte)
-                throw new Exception(
-                    "Value '{$value}' lower than"
-                    . " allowed ({$this->_compareGte})");
-            $value = max($this->_compareGte, $value);
-        }
-        if ($this->_hasLte) {
-            if($value > $this->_compareLte && !$this->_clampLte)
-                throw new Exception(
-                    "Value '{$value}' higher"
-                    . " than allowed ({$this->_compareLte})");
-            $value = min($this->_compareLte, $value);
-        }
-
-        if ($this->_hasGt && $value <= $this->_compareGt)
-            throw new Exception(
-                "Value '{$value}' lower than allowed ({$this->_compareGt})");
-        if ($this->_hasLt && $value >= $this->_compareLt)
-            throw new Exception(
-                "Value '{$value}' higher than allowed ({$this->_compareLt})");
+        $value = $this->checkGte($value);
+        $value = $this->checkLte($value);
+        $this->checkGt($value);
+        $this->checkLt($value);
 
         return $value;
     }
+    
+    /**
+     * Check that the value is within bounds (greater than or equal)
+     * 
+     * @param type $value The value to check
+     * @throws Structr\Exception
+     */
+    protected function checkGte($value)
+    {
+        if ($this->_gte) {
+            if ($value < $this->_gte && !$this->_clampGte) {
+                $this->tooLow($value, $this->_gte);
+            }
+            $value = max($this->_gte, $value);
+        }
+        
+        return $value;
+    }
+    
+    /**
+     * Check that the value is within bounds (less than or equal)
+     * 
+     * @param type $value The value to check
+     * @throws Structr\Exception
+     */
+    protected function checkLte($value)
+    {
+        if ($this->_lte) {
+            if ($value > $this->_lte && !$this->_clampLte) {
+               $this->tooHigh($value, $this->_lte);
+            }
+            $value = min($this->_lte, $value);
+        }
+        
+        return $value;
+    }
+    
+    /**
+     * Check that the value is within bounds (greater than)
+     * 
+     * @param type $value The value to check
+     * @throws Structr\Exception
+     */
+    protected function checkGt($value)
+    {
+        if ($this->_gt && $value <= $this->_gt) {
+            $this->tooLow($value, $this->_gt);
+        }
+    }
+    
+    /**
+     * Check that the value is within bounds (less than)
+     * 
+     * @param type $value The value to check
+     * @throws Structr\Exception
+     */
+    protected function checkLt($value)
+    {
+        if ($this->_lt && $value >= $this->_lt) {
+            $this->tooHigh($value, $this->_lt);
+        }
+    }
+    
+    /**
+     * Error: the value is too high
+     * @throws Structr\Exception
+     */
+    protected function tooHigh($value, $max)
+    {
+         throw new Exception(sprintf(
+            "Value '%s' is higher than allowed (%f)",
+            $value,
+            $max
+        ));
+    }
+    
+    /**
+     * Error: the value is too low
+     * @throws Structr\Exception
+     */
+    protected function tooLow($value, $min)
+    {
+        throw new Exception(sprintf(
+            "Value '%s' is lower than allowed (%f)",
+            $value,
+            $min
+        ));
+    }
 
-    public function setType(&$value) {
-        if ($this->_coerceStrict && !is_numeric($value))
+    /**
+     * {@inheritdoc}
+     */
+    public function setType(&$value)
+    {
+        if ($this->_coerceStrict && !is_numeric($value)) {
             return false;
+        }
+        
         return parent::setType($value);
     }
 }
