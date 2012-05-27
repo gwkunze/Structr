d1f70b950079dff5baef94e49ffe134adcd68c81
Remon van de Kamp <rpkamp@gmail.com>
2012-05-27 19:10:34 +0200

    Added comments to ChoicePrototypeNode and ListPrototypeNode

--- src/Structr/Tree/Composite/ChoicePrototypeNode.php
+++ src/Structr/Tree/Composite/ChoicePrototypeNode.php
@@ -6,8 +6,12 @@ use Structr\Tree\Base\PrototypeNode;
 
 class ChoicePrototypeNode extends PrototypeNode
 {
-
-    public function endPrototype() {
+    /**
+     * Jump back to the parent node of this node
+     * @return \Structr\Tree\Base\Node The parent node of this node
+     */
+    public function endPrototype()
+    {
         return $this->end();
     }
 }
--- src/Structr/Tree/Composite/ListPrototypeNode.php
+++ src/Structr/Tree/Composite/ListPrototypeNode.php
@@ -6,7 +6,12 @@ use Structr\Tree\Base\PrototypeNode;
 
 class ListPrototypeNode extends PrototypeNode
 {
-    public function endItem() {
+    /**
+     * Jump back to the parent node of this node
+     * @return \Structr\Tree\Base\Node The parent node of this node
+     */
+    public function endItem()
+    {
         return $this->parent();
     }
 }
